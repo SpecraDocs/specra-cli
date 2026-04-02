82d4dd5d7bcf0354d6c4c517626747ceb26d71a5
Dalmas Ogembo <dalmasogembo@gmail.com>
2026-04-02 23:23:03 +0300

    chore: version bump, update .gitignore files

--- templates/modern/.svelte-kit/generated/client/nodes/6.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/docs/[product=product]/[version]/[...slug]/+page.svelte";
--- templates/modern/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-import * as server from '../entries/pages/docs/_product_product_/_version_/_layout.server.ts.js';
-
-export const index = 2;
-export { server };
-export const server_id = "src/routes/docs/[product=product]/[version]/+layout.server.ts";
-export const imports = [];
-export const stylesheets = [];
-export const fonts = [];
